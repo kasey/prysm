cb978cbf64fce83403435a54881e648531166040
Kasey Kirkham <kasey@users.noreply.github.com>
2021-12-10 17:28:08 -0600

    removing the get commands for now
    these need some more love so i'd rather leave them out for now

--- cmd/prysmctl/main.go
+++ cmd/prysmctl/main.go
@@ -3,12 +3,9 @@ package main
 import (
 	"os"
 
-	"github.com/prysmaticlabs/prysm/cmd/prysmctl/get"
-
+	"github.com/prysmaticlabs/prysm/cmd/prysmctl/checkpoint"
 	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
-
-	"github.com/prysmaticlabs/prysm/cmd/prysmctl/checkpoint"
 )
 
 var prysmctlCommands []*cli.Command
@@ -25,5 +22,4 @@ func main() {
 
 func init() {
 	prysmctlCommands = append(prysmctlCommands, checkpoint.Commands...)
-	prysmctlCommands = append(prysmctlCommands, get.Commands...)
 }
